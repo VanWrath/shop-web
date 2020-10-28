6bc858fa9f0db7d08411309a472174c6ed44c344
Kyle <31023745+VanWrath@users.noreply.github.com>
2020-10-27 21:50:54 -0400

    update profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,15 +1,48 @@
 // src/components/Profile.js
 
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import Loading from './Loading';
+import config from "../auth_config.json";
 
 const Profile = () => {
-	const { loading, user, isAuthenticated } = useAuth0();
+	const { loading, user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+	const [userMetadata, setUserMetadata] = useState(null);
+
+	useEffect(() => {
+		const getUserMetadata = async () => {
+		  const domain = config.domain;
+	  
+		  try {
+			const accessToken = await getAccessTokenSilently({
+			  audience: `https://${domain}/api/v2/`,
+			  scope: "read:current_user",
+			});
+			
+			const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
+	  
+			const metadataResponse = await fetch(userDetailsByIdUrl, {
+			  headers: {
+				Authorization: `Bearer ${accessToken}`,
+			  },
+			});
+			
+			const { user_metadata } = await metadataResponse.json();
+			console.log("Metadata: "+user_metadata);
+	  
+			setUserMetadata(user_metadata);
+		  } catch (e) {
+			console.log(e.message);
+		  }
+		};
+		
+		getUserMetadata();
+	  }, []);
 
 	if (loading || !user) {
 		return <Loading />;
 	}
+
 
 	return (
 		isAuthenticated && (
@@ -23,6 +56,11 @@ const Profile = () => {
 					<div className="col-9">
 						<h2>{user.name}</h2>
 						<p>{user.email}</p>
+
+						<h3>User Metadata</h3>
+						{userMetadata ? (
+          					<pre>{JSON.stringify(userMetadata, null, 2)}</pre>
+        						) : ("No user metadata defined")}
 
 						<div />
 					</div>
