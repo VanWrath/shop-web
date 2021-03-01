118c45587f811b0f35d9f9d1c4882bf782a45e17
VanWrath <31023745+VanWrath@users.noreply.github.com>
2021-02-28 21:38:06 -0500

    gh-pages

---
+++ build/precache-manifest.e732aa8d5c11261ae2dbfcb5d2ac00dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7d0c6d0892803bf7762",
+    "url": "/shop-web/static/css/main.84097992.chunk.css"
+  },
+  {
+    "revision": "a7d0c6d0892803bf7762",
+    "url": "/shop-web/static/js/main.a7d0c6d0.chunk.js"
+  },
+  {
+    "revision": "c2acd67d4b90f91a2a36",
+    "url": "/shop-web/static/js/1.c2acd67d.chunk.js"
+  },
+  {
+    "revision": "13c41c4505f66efebaab",
+    "url": "/shop-web/static/js/runtime~main.13c41c45.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shop-web/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fcdc69f43bb13bec399e9848c3060988",
+    "url": "/shop-web/index.html"
+  }
+];
